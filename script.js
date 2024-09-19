c627943607ac810c7768d37d94c2d652f4e7f0d0
giovanni <giovannirprovazi@gmail.com>
2024-09-19 11:16:22 -0300

    Criando as variáveis vazias e nulas

--- script.js
+++ script.js
@@ -10,3 +10,13 @@ console.log(nome1)
 
 const nome2="javascript";
 console.log(nome2)
+
+// OUTRAS DECLARAÇÕES 
+
+// declaração indefinida
+let nome3;
+console.log(nome3)
+
+// declaração vazia
+let disciplina =null;
+console.log(disciplina)
